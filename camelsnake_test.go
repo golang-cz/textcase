e63ab7394692787646d57fa1f98b7b9030dda0fa
Vojtech Vitek <vojtech.vitek@visioncritical.com>
2019-03-06 01:50:12 -0500

    Add table driven unit tests

---
+++ camelsnake_test.go
@@ -0,0 +1,27 @@
+package camelsnake
+
+import (
+	"testing"
+)
+
+func TestCamelSnake(t *testing.T) {
+	t.Parallel()
+
+	tt := []struct {
+		in    string
+		camel string
+		snake string
+	}{
+		{in: "Add updated_at to users table", camel: "AddUpdatedAtToUsersTable", snake: "add_updated_at_to_users_table"},
+		{in: "$()&^%(_--crazy__--input$)", camel: "CrazyInput", snake: "crazy_input"},
+	}
+
+	for _, test := range tt {
+		if got := CamelCase(test.in); got != test.camel {
+			t.Errorf("unexpected CamelCase for input(%q), got %q, want %q", test.in, got, test.camel)
+		}
+		if got := SnakeCase(test.in); got != test.snake {
+			t.Errorf("unexpected snake_case for input(%q), got %q, want %q", test.in, got, test.snake)
+		}
+	}
+}
